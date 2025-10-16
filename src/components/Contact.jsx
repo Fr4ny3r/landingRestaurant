787dfaa569bb495e2976be3583d18fc4dcefba91
Fr4ny3r <guevarafranyer09@gmail.com>
2025-10-16 09:56:37 -0400

    f

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ function Contact() {
 
 
   return (
-    <section id="location" className="relative rounded-none md:rounded-xl p-20 bg-[var(--color-card-background)] md:mb-50 z-100 max-h-[600px] container mx-auto text-left">
+    <section id="contact" className="relative rounded-none md:rounded-xl p-10 pt-20 md:p-20 bg-[var(--color-card-background)] md:mb-50 z-100 max-h-[600px] container mx-auto text-left">
       <div className=" w-full h-full flex flex-col md:flex-row justify-around items-center">  
         <span className='text-left absolute top-0 left-0 m-5 mx-8 my-8 p-5 px-8 rounded-xl md:rounded-b-xl -translate-y-1/2 md:-translate-y-1/2 lg:translate-y-0 text-2xl md:text-4xl font-extrabold bg-[var(--color-secondary)]'>Contact</span>
         <form id="form" className="relative w-1/2 flex flex-col w-full md:max-w-[500px] items-start" action="https://formspree.io/f/mayvlrpg" method="POST">
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,34 @@ function Footer() {
 
 
   return (
-    <footer id="footer" className="bg-blue-500">
-      <h1>adasd</h1>
+    <footer id="footer" className="bg-[var(--color-primary)]/60 p-4 xl:p-8 md:mt-50 z-100 max-h-[300px]  text-center md:text-left">
+<div class="footer-container bg-[var(--color-background)] xl:justify-around justify-center items-center text-white xl:flex grid grid-cols-2 grid-rows-2 p-10 rounded-xl">
+    <div class="footer-section">
+        <h3>Delicious Eats</h3>
+        <p>Your destination for the best food.</p>
+    </div>
+
+    <div class="footer-section">
+        <h3>Contact</h3>
+        <p>Address: #### #### 123, Ciudad</p>
+        <p>Phone: +123 456 7890</p>
+        <p>Email: info@deliciouseats.com</p>
+    </div>
+
+    <div class="footer-section hidden xl:block">
+        <h3>Enlaces Rápidos</h3>
+        <ul>
+            <li><a href="#hero">Home</a></li>
+            <li><a href="#menu">Menu</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+    </div>
+
+    <div class="footer-bottom col-span-2 mt-4 text-center">
+        <p>&copy; 2025 Delicious Eats. Todos los derechos reservados.</p>
+        </div>
+</div>
     </footer>
   );
 }
